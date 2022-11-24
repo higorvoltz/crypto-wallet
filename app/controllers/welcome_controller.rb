0efd41458b00a0ee9f8392516818296c42360670
higorvoltz <higor.amorim@voltzmotors.com>
2022-11-24 14:54:34 -0300

    add sessio storage and cookies

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@
 # returns welcome screen
 class WelcomeController < ApplicationController
   def index
+    # cookies[:curso] = 'Curso de ruby on rails'
     @name = params[:name].nil? ? 'Higor' : params[:name]
   end
 end
